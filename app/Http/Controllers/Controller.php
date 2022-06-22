1dcd4d6f7a9ac7891a167f9e2afcfae9f9f14f23
Huynh <nghuynh.dev@gmail.com>
2022-06-22 14:13:15 +0700

    Setup project

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function sentResponse($data ='', $message='success',$status = 200){
+    return response()->json([
+        'message' => $message,
+        'data' => $data,
+    ],$status);
+    }
+}
