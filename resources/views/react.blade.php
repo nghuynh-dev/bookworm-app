af57a60970b30cced15429e1a64c92a3a96a0b86
Huynh <nghuynh.dev@gmail.com>
2022-06-28 15:00:54 +0700

    feat: mix reactjs to project

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Welcome to Book Worm</title>
     </head>
-    <body class="antialiased">
+    <body>
         <div id="root"></div>
         <script src="{{mix('/js/app.js')}}"></script>
     </body>
