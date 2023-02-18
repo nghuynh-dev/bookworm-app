1a93cf2e659a146fd2c683b58a2c71f88533f7ac
Huynh Nguyen <nghuynh.dev@gmail.com>
2023-02-18 19:31:23 +0700

    Fix(fe): Refactor error component

--- resources/js/components/error/index.js
+++ resources/js/components/error/index.js
@@ -1,5 +1,13 @@
-export default function ErrorComponent() {
+import React from 'react';
+import './style.css';
+
+const ErrorComponent = () => {
     return (
-        <h1>404 NOT FOUND</h1>
+        <div className="not-found">
+            <h1 className="error-404">404</h1>
+            <p className="error-desc">The page you're looking for cannot be found.</p>
+        </div>
     );
-}
+};
+
+export default ErrorComponent;
