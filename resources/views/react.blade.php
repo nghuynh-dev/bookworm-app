052b246c15fb7ffc8d21846db11b9f40bfc6e4ff
Huynh Nguyen <nghuynh.dev@gmail.com>
2023-02-19 01:00:39 +0700

    Support SEO before deploy

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -3,7 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Welcome to Book Worm</title>
+        <title>A low-cost book by Huynh Nguyen</title>
+        <meta name="description" content="A low-cost book by Huynh Nguyen">
+        <link rel="icon" type="image/x-icon" href="../assets/shop.ico">
     </head>
     <body>
         <div id="root"></div>
